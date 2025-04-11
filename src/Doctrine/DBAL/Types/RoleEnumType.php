6ceb1be4889c9ad22728f7f52a8d5c075723771f
Matilda-bit <polinaovras@gmail.com>
2025-04-11 15:45:16 +0300

    roles updated

--- src/Doctrine/DBAL/Types/RoleEnumType.php
+++ src/Doctrine/DBAL/Types/RoleEnumType.php
@@ -5,7 +5,7 @@ class RoleEnumType extends AbstractEnumType
 {
     const ROLE_ENUM = 'role_enum';
 
-    protected $values = ['USER', 'ADMIN'];
+    protected $values = ['USER', 'ADMIN', 'REP'];
 
     public static function create()
     {
