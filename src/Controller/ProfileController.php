89a6a9842c62040f3454e290983f2983852d4004
Matilda-bit <polinaovras@gmail.com>
2025-04-19 16:24:04 +0300

    fix user profile

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -2,47 +2,30 @@
 
 namespace App\Controller;
 
-use App\Entity\User;
 use App\Entity\Trade;
+use Doctrine\ORM\EntityManagerInterface; 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\User\UserInterface;
-//todo: refactor userController?
 
-#[IsGranted('ROLE_USER')]
 class ProfileController extends AbstractController
 {
     #[Route('/user/dashboard', name: 'user_dashboard', methods: ['GET'])]
-    public function index()
+    public function index(EntityManagerInterface $em, UserInterface $user)
     {
-        $user = $this->getUser();
 
         if (!$user) {
             $this->addFlash('login_error', 'Your session expired, please login again');
             return $this->redirectToRoute('app_login_register');
         }
 
-        $trades = $this->getDoctrine()->getRepository(Trade::class)->findBy(['user' => $user]);
+        $trades = $em->getRepository(Trade::class)->findBy(['user' => $user]);
 
         $template = '/dashboard/user/profile.html.twig';
 
-        // dd($trades);
-
         return $this->render($template, [
             'user' => $user,
-            'trades' => $trades,
-        ]);
-    }
-
-    #[Route('/profile', name: 'profile', methods: ['GET'])]
-    public function profile(UserInterface $user)
-    {
-        $userData = $this->getDoctrine()->getRepository(User::class)->find($user->getId());
-        $trades = $this->getDoctrine()->getRepository(Trade::class)->findBy(['user' => $userData, 'status' => 'open']);
-        
-        return $this->render('dashboard/user/profile.html.twig', [
-            'user' => $userData,
             'trades' => $trades,
         ]);
     }
